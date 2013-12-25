f1c2083637d6f3e2c358d56ab1f9c61d0c9f8d14
Andrew <apurpura@zagmail.gonzaga.edu>
2013-12-24 16:24:28 -0800

    test

--- phonegap-start-master/www/js/index.js
+++ phonegap-start-master/www/js/index.js
@@ -48,15 +48,6 @@ function loadRepoDetail(owner,name) {
     });
 }
 
-$(document).bind("mobileinit", function()
-{
-    if (navigator.userAgent.indexOf("Android") != -1)
-    {
-        $.mobile.defaultPageTransition = 'none';
-        $.mobile.defaultDialogTransition = 'none';
-    }
-});
-
 
 //database
 
